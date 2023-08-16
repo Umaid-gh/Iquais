2d2624441659ae3523bcf44b8daab0d5d018d11f
Umaid-gh <mailtoumaid@gmail.com>
2023-08-17 05:22:41 +0530

    Added Swagger

--- src/main/java/com/aiykr/iquais/config/SwaggerConfig.java
+++ src/main/java/com/aiykr/iquais/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.aiykr.iquais.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -9,8 +11,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+@EnableWebMvc
 @EnableSwagger2
+@Component
 public class SwaggerConfig {
 
     @Bean
