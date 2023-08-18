fa9deb824fabbbfaa25143670f7451a849d2ab73
Umaid-gh <mailtoumaid@gmail.com>
2023-08-19 05:22:09 +0530

    methods introduced

--- src/main/java/com/aiykr/iquais/service/impl/UserServiceImpl.java
+++ src/main/java/com/aiykr/iquais/service/impl/UserServiceImpl.java
@@ -11,7 +11,6 @@ import com.aiykr.iquais.exception.IquaisException;
 import com.aiykr.iquais.repository.UserRepository;
 import com.aiykr.iquais.service.EmailService;
 import com.aiykr.iquais.service.UserService;
-import org.apache.commons.lang3.StringUtils;
 import org.bson.types.ObjectId;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
@@ -19,13 +18,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -36,11 +33,16 @@ public class UserServiceImpl implements UserService {
 
     private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
+    private static final String ERROR_CODE_IQ001 = "IQ001";
+
     @Autowired
     UserRepository userRepository;
 
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     /**
      * Creates a new user based on the provided information.
@@ -50,57 +52,17 @@ public class UserServiceImpl implements UserService {
      * @throws IquaisException If an error occurs during user creation.
      */
     // define levels of log
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Transactional(rollbackFor = {IquaisException.class, EmailSendingException.class, Exception.class})
     public Response<UserResponseDTO> createUser(PostUserDTO postUserDTO) throws IquaisException {
         Response<UserResponseDTO> response = new Response<>();
         try {
-            // Check if the student is already present by email address
-            String studentEmail = postUserDTO.getEmail();
-            log.info("Student Email: {}", studentEmail);
+            UserDAO studentDAO = createStudent(postUserDTO);
+            UserDAO guardianDAO = createGuardian(postUserDTO);
+            log.info("studentDAO : {}", studentDAO);
+            log.info("guardianDAO : {}", guardianDAO);
+            sendEmails(postUserDTO.getEmail(), postUserDTO.getGuardianEmail());
 
-            Optional<UserDAO> studentUserOpt = userRepository.findByEmail(studentEmail);
-
-            UserDAO studentDAO = null;
-            if (studentUserOpt.isPresent()) {
-                String email = studentUserOpt.get().getEmail();
-
-                if (!StringUtils.isEmpty(email) && email.equalsIgnoreCase(studentEmail)) {
-                    log.info("Student Email {} already present in our DB", studentEmail);
-                    throw new IquaisException(HttpStatus.CONFLICT, "IQ002", "Student Email already present in our DB");
-                }
-            }
-            // Create Student
-            studentDAO = new ModelMapper().map(postUserDTO, UserDAO.class);
-            studentDAO.setType(UserType.STUDENT.name());
-            userRepository.save(studentDAO);
-            log.info("Save New Student: {}", studentDAO);
-
-            // Check if the guardian is already present by email address
-            String guardianEmail = postUserDTO.getGuardianEmail();
-            log.info("Guardian Email: {}", guardianEmail);
-            Optional<UserDAO> guardianUserOpt = userRepository.findByEmail(guardianEmail);
-            UserDAO guardianDao = null;
-            if (guardianUserOpt.isPresent()) {
-                String email = guardianUserOpt.get().getEmail();
-                log.info("Guardian Email from DB: {}", email);
-                if (StringUtils.isEmpty(email) && email.equalsIgnoreCase(guardianEmail))
-                    log.info("Guardian Email {} already present in our DB", guardianEmail);
-            }
-            else {
-                // Create Guardian
-                UserDAO guardianDAO = new UserDAO();
-                guardianDAO.setFirstName(postUserDTO.getGuardianFirstName());
-                guardianDAO.setLastName(postUserDTO.getGuardianLastName());
-                guardianDAO.setEmail(postUserDTO.getGuardianEmail());
-                guardianDAO.setType(UserType.GUARDIAN.name());
-                userRepository.save(guardianDAO);
-                log.info("Save New Guardian: {}", guardianDAO);
-            }
-            // Send email to student and guardian
-            sendEmailTo(postUserDTO.getEmail(), postUserDTO.getGuardianEmail());
-
-            // Generate and send the response
-            UserResponseDTO userResponseDTO = new ModelMapper().map(postUserDTO, UserResponseDTO.class);
+            UserResponseDTO userResponseDTO = modelMapper.map(postUserDTO, UserResponseDTO.class);
             response.setMeta(Meta.builder().statusCode(HttpStatus.CREATED.value()).message("Student Created Successfully").build());
             response.setData(userResponseDTO);
             return response;
@@ -108,21 +70,76 @@ public class UserServiceImpl implements UserService {
             throw iqEx;
         } catch (Exception ex) {
             log.error("An exception occurred: {}", ex.getMessage(), ex);
-            ex.getStackTrace();
-            throw new IquaisException(HttpStatus.INTERNAL_SERVER_ERROR, "IQ001", "Exception occurred while generating and storing in DB");
+            throw new IquaisException(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_CODE_IQ001, "Exception occurred while generating and storing in DB");
         }
     }
 
     /**
-     * Sends an email to the student and guardian with account creation and linking information.
+     * Creates a new student user based on the provided information.
+     *
+     * @param postUserDTO The data required to create the student.
+     * @return The UserDAO representing the newly created student.
+     * @throws IquaisException If an error occurs during student creation.
+     */
+    private UserDAO createStudent(PostUserDTO postUserDTO) throws IquaisException {
+        // Check if student already exists
+        try {
+            Optional<UserDAO> studentUserOpt = userRepository.findByEmail(postUserDTO.getEmail());
+            if (studentUserOpt.isPresent()) {
+                log.info("Student Email {} already present in our DB", studentUserOpt.get().getEmail());
+                throw new IquaisException(HttpStatus.CONFLICT, "IQ002", "Student Email already present in our DB");
+            }
+            UserDAO studentDAO = modelMapper.map(postUserDTO, UserDAO.class);
+            studentDAO.setType(UserType.STUDENT.name());
+            userRepository.save(studentDAO);
+            log.info("StudentDAO saved in DB");
+            return studentDAO;
+        } catch (IquaisException iqEx) {
+            throw iqEx;
+        } catch (Exception ex) {
+            log.error("An exception occurred while creating student: {}", ex.getMessage(), ex);
+            throw new IquaisException(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_CODE_IQ001, "Exception occurred while creating student");
+        }
+    }
+
+    /**
+     * Creates a new guardian user if not already present based on the provided information.
+     *
+     * @param postUserDTO The data required to create the guardian.
+     * @return The UserDAO representing the newly created guardian, or null if the guardian already exists.
+     */
+    private UserDAO createGuardian(PostUserDTO postUserDTO) throws IquaisException {
+        // Check if guardian already exists
+        try {
+            Optional<UserDAO> guardianUserOpt = userRepository.findByEmail(postUserDTO.getGuardianEmail());
+            if (guardianUserOpt.isEmpty()) {
+                UserDAO guardianDAO = new UserDAO();
+                guardianDAO.setFirstName(postUserDTO.getGuardianFirstName());
+                guardianDAO.setLastName(postUserDTO.getGuardianLastName());
+                guardianDAO.setEmail(postUserDTO.getGuardianEmail());
+                guardianDAO.setType(UserType.GUARDIAN.name());
+                userRepository.save(guardianDAO);
+                log.info("GuardianDAO saved in DB");
+                return guardianDAO;
+            }
+            log.info("Guardian Email {} already present in our DB", postUserDTO.getGuardianEmail());
+            return null;
+        } catch (Exception ex) {
+            log.error("An exception occurred while creating guardian: {}", ex.getMessage(), ex);
+            throw new IquaisException(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_CODE_IQ001, "Exception occurred while creating guardian");
+        }
+    }
+
+    /**
+     * Sends welcome emails to the student and guardian after account creation.
      *
      * @param studentEmail  The email address of the student.
      * @param guardianEmail The email address of the guardian.
      * @throws IquaisException If an error occurs while sending the email.
      */
-    private void sendEmailTo(String studentEmail, String guardianEmail) throws IquaisException {
+    private void sendEmails(String studentEmail, String guardianEmail) throws IquaisException {
         try {
-            emailService.sendEmail(studentEmail, guardianEmail, "Student Account Created and Guardian Linked", "Welcome to our platform! \nYour password will be sent in separate email!!");
+            emailService.sendEmail(studentEmail, guardianEmail, "Student Account Created", "Welcome to our platform! \nGuardian Linked and your password will be sent in separate email!!");
             log.info("Email sent successfully!!");
         } catch (MessagingException msgEx) {
             // Handle exception properly in production code
@@ -146,8 +163,10 @@ public class UserServiceImpl implements UserService {
             UserDAO userDAO = optionalUserDAO.get();
             userResponseDTO = new ModelMapper().map(userDAO, UserResponseDTO.class);
             response.setData(userResponseDTO);
+            log.info("User {} present in our DB", userResponseDTO.getFirstName());
             response.setMeta(Meta.builder().statusCode(HttpStatus.FOUND.value()).message("Student Retrieved Successfully").build());
         } else {
+            log.info("User is not present in our DB");
             response.setMeta(Meta.builder().statusCode(HttpStatus.NOT_FOUND.value()).message("Student Not Found in DB").build());
         }
         log.info("User Detail: {}", userResponseDTO);
@@ -164,12 +183,13 @@ public class UserServiceImpl implements UserService {
         Response<List<UserResponseDTO>> response = new Response<>();
         List<UserDAO> userDAOS = userRepository.findAll();
         List<UserResponseDTO> usersList = new ArrayList<>();
-        ModelMapper modelMapper = new ModelMapper();
+        this.modelMapper = new ModelMapper();
         userDAOS.forEach(userDAO -> usersList.add(modelMapper.map(userDAO, UserResponseDTO.class)));
         if (usersList.isEmpty())
             response.setMeta(Meta.builder().message("DataBase is Empty").statusCode(HttpStatus.NOT_FOUND.value()).build());
         else
             response.setMeta(Meta.builder().message("Retrieval Successful").statusCode(HttpStatus.OK.value()).build());
+        log.info("Total Users: {}",usersList.size());
         log.info("Users Details: {}", usersList);
         response.setData(usersList);
         return response;
