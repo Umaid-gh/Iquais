f5e42f9cb5585152cf6437b8a21c179242bad140
Umaid-gh <mailtoumaid@gmail.com>
2023-08-31 03:28:33 +0530

    minor changes

--- src/main/java/com/aiykr/iquais/IquaisApplication.java
+++ src/main/java/com/aiykr/iquais/IquaisApplication.java
@@ -1,8 +1,10 @@
 package com.aiykr.iquais;
 
 import com.aiykr.iquais.config.SwaggerConfig;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -19,5 +21,10 @@ public class IquaisApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(IquaisApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
